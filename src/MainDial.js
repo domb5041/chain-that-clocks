162bba0e5474660bdee42586f44266c857d1c246
dominicbutler <dominic.butler@exonar.com>
2021-03-04 22:31:34 +0000

    basic clock with subdials

--- src/MainDial.js
+++ src/MainDial.js
@@ -1,5 +1,56 @@
+import React, { useState, useEffect } from 'react';
 import * as styled from './MainDial.styled';
 
 export default function MainDial() {
-    return <styled.MainDial></styled.MainDial>;
+    const [time, setTime] = useState([0, 0, 0]);
+    const [timeA, setTimeA] = useState([0, 0, 0]);
+    const [timeB, setTimeB] = useState([0, 0, 0]);
+
+    useEffect(() => {
+        setInterval(updateClock, 1000);
+        setInterval(updateClockA, 1000);
+        setInterval(updateClockB, 1000);
+    }, []);
+
+    const updateClock = () => {
+        const d = new Date();
+        const h = d.getHours() * 30;
+        const m = d.getMinutes() * 6;
+        const s = d.getSeconds() * 6;
+        setTime([h, m, s]);
+    };
+
+    const updateClockA = () => {
+        const d = new Date();
+        const h = d.getHours() * 30;
+        const m = d.getMinutes() * 6;
+        const s = d.getSeconds() * 6;
+        setTimeA([h, m, s]);
+    };
+
+    const updateClockB = () => {
+        const d = new Date();
+        const h = d.getHours() * 30;
+        const m = d.getMinutes() * 6;
+        const s = d.getSeconds() * 6;
+        setTimeB([h, m, s]);
+    };
+
+    return (
+        <styled.MainDial>
+            <styled.HandH time={time} />
+            <styled.HandM time={time} />
+            <styled.HandS time={time} />
+            <styled.SubDialA>
+                <styled.HandHs time={timeA} />
+                <styled.HandMs time={timeA} />
+            </styled.SubDialA>
+            <styled.SubDialB>
+                <styled.HandHs time={timeB} />
+                <styled.HandMs time={timeB} />
+            </styled.SubDialB>
+        </styled.MainDial>
+    );
 }
+
+// style={{ transform: `translateX(-50%) rotate(${time[2]}deg)` }}
--- src/MainDial.styled.js
+++ src/MainDial.styled.js
@@ -4,5 +4,63 @@ export const MainDial = styled.div`
     width: 500px;
     height: 500px;
     border-radius: 100%;
+    border: 4px solid white;
+    position: relative;
+`;
+
+export const SubDial = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 100%;
     border: 2px solid white;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+export const SubDialA = styled(SubDial)`
+    left: 15px;
+`;
+
+export const SubDialB = styled(SubDial)`
+    right: 15px;
+`;
+
+const Hand = styled.div`
+    position: absolute;
+    background-color: white;
+    bottom: 50%;
+    left: 50%;
+    transform-origin: center bottom;
+    border-radius: 50px;
+`;
+
+export const HandH = styled(Hand)`
+    width: 15px;
+    height: 160px;
+    transform: ${props => `translateX(-50%) rotate(${props.time[0]}deg)`};
+`;
+
+export const HandM = styled(Hand)`
+    width: 10px;
+    height: 220px;
+    transform: ${props => `translateX(-50%) rotate(${props.time[1]}deg)`};
+`;
+
+export const HandS = styled(Hand)`
+    width: 5px;
+    height: 240px;
+    transform: ${props => `translateX(-50%) rotate(${props.time[2]}deg)`};
+`;
+
+export const HandHs = styled(Hand)`
+    width: 6px;
+    height: 60px;
+    transform: ${props => `translateX(-50%) rotate(${props.time[0]}deg)`};
+`;
+
+export const HandMs = styled(Hand)`
+    width: 6px;
+    height: 90px;
+    transform: ${props => `translateX(-50%) rotate(${props.time[1]}deg)`};
 `;
